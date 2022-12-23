0464855afc740c30717af064ca4f0b196572ba1a
yassine <ahmed.yassin.elhamzaoui2019@gmail.com>
2022-12-23 12:44:03 +0100

    add a function to show all answears

--- public/assets/script.js
+++ public/assets/script.js
@@ -18,11 +18,21 @@ let image=document.querySelector("#image");
 
 cercle[0].style.background="#015958";
 cercle[0].style.color="#C7FFED";
+
+let answers=document.querySelectorAll(".answers");
+
+let Results=[];
+let Answer;
+let checkanswer=[];
+let RightAnswers=[];
+let Rightexplanation=[];
+
 btnNext.onclick=()=>{
    if(compt<2){
     compt++;
     if(compt==2){
         btnNext.style.display="none";
+        corectAnswears();
     }
     if(compt==1){
         Getdata();
@@ -83,6 +93,13 @@ while(x--){
 // the progress bar
 let incrementRange=document.querySelector(".increment-range");
 let index=0;
+let radioQuestion=document.querySelectorAll(".radio-question");
+let checkQuestion=document.querySelectorAll(".check-question");
+let score=document.querySelector(".scrore");
+let container=document.querySelector(".container");
+let scoreNumber=document.querySelector(".score-number");
+let scoreStaut=document.querySelector(".score-staut");
+let allAnswears=document.querySelector(".all-answears");
 function Getdata()
 {
  var  mydata=new XMLHttpRequest();
@@ -94,28 +111,78 @@ function Getdata()
         showdata(quizeObject[tab1[index]],numberOfquestion);
         Countdown(20,numberOfquestion);
         incrementRange.style.width=`${(index+1)*10}%`;
-        nextQuestion.onclick=()=>{
+        totalQuestion.innerHTML=numberOfquestion;
+        curentQuestion.innerHTML=index+1;
+        nextQuestion.onclick=()=>{ 
+            if(index==numberOfquestion-2){
+                nextQuestion.innerHTML="finish";
+            }else if(index==numberOfquestion-1){
+                score.style.display="block";
+                container.style.display="none";
+                btnNext.style.visibility="visible";
+                btnNext.innerHTML="show all ansewers";
+                nextQuestion.style.visibility="hidden";
+                scoreNumber.innerHTML+=scoreresult(quizeObject)+"/10";
+                if(scoreresult(quizeObject)==10){
+                    scoreStaut.innerHTML+="Exelent";
+                }else if(scoreresult(quizeObject)<10 && scoreresult(quizeObject)>=7){
+                    scoreStaut.innerHTML+="Good";
+                }else if(scoreresult(quizeObject)<7 && scoreresult(quizeObject)>=5){
+                    scoreStaut.innerHTML+="tolerable";
+                }else if(scoreresult(quizeObject)<5 && scoreresult(quizeObject)>=3){
+                    scoreStaut.innerHTML+="weak";
+                }else{
+                    scoreStaut.innerHTML+="very weak";
+                }
+            }
+            if(index==1 || index==6  || index==8){
+                for(let j=0;j<4;j++){
+                    if(checkQuestion[j].checked==true){
+                        Answer=quizeObject[tab1[index]]["Réponse"+(j+1)];
+                        checkanswer.push(Answer);
+                        checkQuestion[j].checked=false;
+                    }
+                }
+                Results.push(checkanswer);
+                checkanswer=[];
+            }else{
+                let x=0;
+                for(let j=0;j<4;j++){
+                    if(radioQuestion[j].checked==true){
+                        Answer=quizeObject[tab1[index]]["Réponse"+(j+1)];
+                        Results.push(Answer);
+                        radioQuestion[j].checked=false;
+                        x++;
+                }      
+            }
+            if(x==0){
+                Results.push("empty");
+            }
+            }
+            console.log(index)               
+            console.log(Results);
             index++;
             clearInterval(counInterval);
             Countdown(20,numberOfquestion);
             if(index<numberOfquestion){
+                curentQuestion.innerHTML=index+1;
                 incrementRange.style.width=`${(index+1)*10}%`;
-            }
-            if(index==numberOfquestion-1){
-                nextQuestion.style.display="none";
-                btnNext.style.visibility="visible";
-                btnNext.innerHTML="Show Results";
             }
             showdata(quizeObject[tab1[index]],numberOfquestion);
         }
     }
+
+
    }
    mydata.open('GET',"../assets/data.json",true);
    mydata.send();
 }
-let radioQuestion=document.querySelectorAll(".radio-question");
-   
-let checkQuestion=document.querySelectorAll(".check-question");
+let curentQuestion=document.querySelector(".curent-question");
+let totalQuestion=document.querySelector(".total-question");
+
+
+
+
 for(let i=0;i<4;i++){
     checkQuestion[i].style.display="none";
 }
@@ -126,24 +193,30 @@ function showdata(obj,allquestions){
     let answearB=document.querySelector("#Answear-b");
     let answearC=document.querySelector("#Answear-c");
     let answearD=document.querySelector("#Answear-d");
+    let myQuestion=document.querySelector(".my-Question");
 
   
     if(index==1 || index==6  || index==8){
         for(let i=0;i<4;i++){
             radioQuestion[i].style.display="none";
             checkQuestion[i].style.display="block";
+            answers[i].setAttribute("for",`yes${i}`);
         }
     }else{
         for(let i=0;i<4;i++){
             radioQuestion[i].style.display="block";
             checkQuestion[i].style.display="none";
+            answers[i].removeAttribute("for",`yes${i}`);
         }
     }
     if(index<allquestions){
-        answearA.innerHTML=obj['RéponseA'];
-        answearB.innerHTML=obj['RéponseB'];
-        answearC.innerHTML=obj['RéponseC'];
-        answearD.innerHTML=obj['RéponseD'];
+        answearA.innerHTML=obj['Réponse1'];
+        answearB.innerHTML=obj['Réponse2'];
+        answearC.innerHTML=obj['Réponse3'];
+        answearD.innerHTML=obj['Réponse4'];
+        myQuestion.innerHTML=obj["Quesion"];
+        Rightexplanation.push(obj['explanation']);
+        RightAnswers.push(obj['RightEsponse']);
     }
 }
 
@@ -154,7 +227,6 @@ function Countdown(timeing,allquestions){
         counInterval=setInterval(function(){
             if(timeing<10){
             dateCrono.innerHTML=`0${timeing}`;
-
             }else{
                 dateCrono.innerHTML=timeing;
             }
@@ -163,17 +235,51 @@ function Countdown(timeing,allquestions){
            }else if(timeing<4){
             insidecircle.style.background=`conic-gradient(red ${timeing * 18}deg,#dedede 0)`;
            }else{
-            insidecircle.style.background=`conic-gradient(green ${timeing * 18}deg,#dedede 0)`;
+            insidecircle.style.background=`conic-gradient(#1e3a8a ${timeing * 18}deg,#dedede 0)`;
            }
            timeing--;
            if(timeing<0){
             clearInterval(counInterval);
-            nextQuestion.click();
+             if(index==allquestions-1){
+                nextQuestion.style.visibility="hidden";
+                btnNext.style.visibility="visible";
+                btnNext.innerHTML="Show Results";
+             }else{
+                nextQuestion.click();
+             }
            }
-
-
         },1000)
     }
     
 
 }
+function scoreresult(obj){
+    let temp=0;
+    for(let k=0;k<Results.length;k++){
+        if(Results[k]==obj[tab1[k]].RightEsponse){
+            temp++;
+        }
+    }
+    return temp;
+}
+function corectAnswears(){
+    for(let r=0;r<Results.length;r++){
+        if(RightAnswers[r]==Results[r]){
+            allAnswears.innerHTML+=`
+            <p class="font-bold flex flex-start text-xl">The Question:${r+1}</p>
+            <p class="green-answear flex flex-start">your answear: ${Results[r]}</p>
+            <p class="green-answear flex flex-start">RightAnswer : ${RightAnswers[r]}</p>
+            <p class="blue-exiplanation flex flex-start ">explanation : ${Rightexplanation[r]}</p>`;
+        }else{
+            allAnswears.innerHTML+=`
+            <p class="font-bold flex flex-start text-xl">The Question:${r+1}</p>
+            <p class="red-answear flex flex-start">your answear: ${Results[r]}</p>
+            <p class="green-answear flex flex-start">RightAnswer : ${RightAnswers[r]}</p>
+            <p class="blue-exiplanation flex flex-start">explanation : ${Rightexplanation[r]}</p>`;
+        }
+        
+    }
+}
+
+
+
